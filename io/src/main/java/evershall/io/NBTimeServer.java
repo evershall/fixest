b1d77157fc80f91a6c7336482cb2c50ca81c130f
Nik <nik@evershall.com>
2015-10-22 14:59:31 +0100

    minor

--- io/src/main/java/evershall/io/NBTimeServer.java
+++ io/src/main/java/evershall/io/NBTimeServer.java
@@ -30,13 +30,17 @@ package evershall.io;
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
-import java.nio.channels.spi.*;
-import java.net.*;
-import java.util.*;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
 
 /* A non-blocking Internet time server implemented using
  * the I/O (NIO) facilities added to Java SE 1.4.
@@ -44,75 +48,76 @@ import java.util.*;
 
 // Listen on a port for connections and write back the current time.
 public class NBTimeServer {
-	private static final int DEFAULT_TIME_PORT = 8900;
+   private static final int DEFAULT_TIME_PORT = 8900;
 
-	// Constructor with no arguments creates a time server on default port.
-	public NBTimeServer() throws Exception {
-		acceptConnections(this.DEFAULT_TIME_PORT);
-	}
+   // Constructor with no arguments creates a time server on default port.
+   public NBTimeServer() throws Exception {
+      acceptConnections(DEFAULT_TIME_PORT);
+   }
 
-	// Constructor with port argument creates a time server on specified port.
-	public NBTimeServer(int port) throws Exception {
-		acceptConnections(port);
-	}
+   // Constructor with port argument creates a time server on specified port.
+   public NBTimeServer(final int port) throws Exception {
+      acceptConnections(port);
+   }
 
-	// Accept connections for current time. Lazy Exception thrown.
-	private static void acceptConnections(int port) throws Exception {
-		// Selector for incoming time requests
-		Selector acceptSelector = SelectorProvider.provider().openSelector();
+   // Accept connections for current time. Lazy Exception thrown.
+   private static void acceptConnections(final int port) throws Exception {
+      // Selector for incoming time requests
+      final Selector acceptSelector = SelectorProvider.provider().openSelector();
 
-		// Create a new server socket and set to non blocking mode
-		ServerSocketChannel ssc = ServerSocketChannel.open();
-		ssc.configureBlocking(false);
+      // Create a new server socket and set to non blocking mode
+      final ServerSocketChannel ssc = ServerSocketChannel.open();
+      ssc.configureBlocking(false);
 
-		// Bind the server socket to the local host and port
+      // Bind the server socket to the local host and port
 
-		InetAddress lh = InetAddress.getLocalHost();
-		InetSocketAddress isa = new InetSocketAddress(lh, port);
-		ssc.socket().bind(isa);
+      final InetAddress lh = InetAddress.getLocalHost();
+      final InetSocketAddress isa = new InetSocketAddress(lh, port);
+      ssc.socket().bind(isa);
 
-		// Register accepts on the server socket with the selector. This
-		// step tells the selector that the socket wants to be put on the
-		// ready list when accept operations occur, so allowing multiplexed
-		// non-blocking I/O to take place.
-		SelectionKey acceptKey = ssc.register(acceptSelector, SelectionKey.OP_ACCEPT);
+      // Register accepts on the server socket with the selector. This
+      // step tells the selector that the socket wants to be put on the
+      // ready list when accept operations occur, so allowing multiplexed
+      // non-blocking I/O to take place.
+      final SelectionKey acceptKey = ssc.register(acceptSelector, SelectionKey.OP_ACCEPT);
 
-		int keysAdded = 0;
+      int keysAdded = 0;
 
-		// Here's where everything happens. The select method will
-		// return when any operations registered above have occurred, the
-		// thread has been interrupted, etc.
-		while ((keysAdded = acceptSelector.select()) > 0) {
-			// Someone is ready for I/O, get the ready keys
-			Set readyKeys = acceptSelector.selectedKeys();
-			Iterator i = readyKeys.iterator();
+      // Here's where everything happens. The select method will
+      // return when any operations registered above have occurred, the
+      // thread has been interrupted, etc.
+      while ((keysAdded = acceptSelector.select()) > 0) {
+         // Someone is ready for I/O, get the ready keys
+         final Set readyKeys = acceptSelector.selectedKeys();
+         final Iterator i = readyKeys.iterator();
 
-			// Walk through the ready keys collection and process date requests.
-			while (i.hasNext()) {
-				SelectionKey sk = (SelectionKey) i.next();
-				i.remove();
-				// The key indexes into the selector so you
-				// can retrieve the socket that's ready for I/O
-				ServerSocketChannel nextReady = (ServerSocketChannel) sk.channel();
-				// Accept the date request and send back the date string
-				Socket s = nextReady.accept().socket();
-				// Write the current time to the socket
-				PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-				Date now = new Date();
-				out.println(now);
-				out.close();
-			}
-		}
-	}
+         // Walk through the ready keys collection and process date requests.
+         while (i.hasNext()) {
+            final SelectionKey sk = (SelectionKey) i.next();
+            i.remove();
+            // The key indexes into the selector so you
+            // can retrieve the socket that's ready for I/O
+            final ServerSocketChannel nextReady = (ServerSocketChannel) sk.channel();
+            // Accept the date request and send back the date string
+            final Socket s = nextReady.accept().socket();
+            // Write the current time to the socket
+            final PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            final Date now = new Date();
+            out.println(now);
+            out.close();
+         }
+      }
+   }
 
-	// Entry point.
-	public static void main(String[] args) {
-		// Parse command line arguments and
-		// create a new time server (no arguments yet)
-		try {
-			NBTimeServer nbt = new NBTimeServer();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+   // Entry point.
+   public static void main(final String[] args) {
+      // Parse command line arguments and
+      // create a new time server (no arguments yet)
+      try {
+         final NBTimeServer nbt = new NBTimeServer();
+      }
+      catch (final Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
